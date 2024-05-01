4c7ca8da9ef9ce58d62301dbaba96eff1feb1063
Ayoze Gómez Rosa <130746651+ayozegr05@users.noreply.github.com>
2024-05-01 19:01:34 +0100

    Update config.js

--- Firebase/config.js
+++ Firebase/config.js
@@ -1,8 +1,1 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyB5V7SNVe8xdwtr-dKqdd8PyFiRAZq6PkQ",
-  authDomain: "ayozegrbubbodb.firebaseapp.com",
-  projectId: "ayozegrbubbodb",
-  storageBucket: "ayozegrbubbodb.appspot.com",
-  messagingSenderId: "69332785581",
-  appId: "1:69332785581:web:6da6466acf6322e9d2a924"
-};
+
