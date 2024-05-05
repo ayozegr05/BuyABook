9d1c0dafe1105c52eb0f40243314e24b0b380493
ayozegr05 <ayozegr05@gmail.com>
2024-05-05 18:52:28 +0000

    Mongo db installed, models and routes starting

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import morgan from 'morgan';
-import router from './Api/Routes/index.js';
+import router from './DbMongo/routes.js';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';  
+import { connectDB } from './DbMongo/db.js';
+import dotenv from 'dotenv';
+
 // import exphbs from 'express-handlebars'
 import path from 'path';
+
+dotenv.config({ path: 'config.env' });
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -22,6 +27,10 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use(router);
+
+// Llama a la función para conectar a MongoDB
+connectDB();
+
 // app.use(express.static(join(__dirname, 'public'))); 
 
 export default app;
